858444966c69141c516f36c43152f9da61f0c979
OscarB <oballesteros@abstract.cl>
2019-06-11 22:35:54 -0400

    add auth and navigation

---
+++ src/components/Navigations/Guest.vue
@@ -0,0 +1,7 @@
+<template>
+    <b-nav tabs>
+        <b-nav-item exact to="/login">
+            Iniciar Sesión
+        </b-nav-item>
+    </b-nav>
+</template>
---
+++ src/components/Navigations/Logged.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-nav tabs>
+        <b-nav-item exact to="/secret">
+            Página de Usuario registrado
+        </b-nav-item>
+        <b-nav-item exact to="/secret/notes">
+            Notas
+        </b-nav-item>
+        <b-nav-item @click="_logout">
+            Cerrar Sesión
+        </b-nav-item>
+    </b-nav>
+</template>
+<script>
+    import {mapMutations} from 'vuex'
+    export default {
+        methods: {
+            ...mapMutations('auth', ['logout']),
+            _logout() {
+                this.logout()
+                this.$router.push('/login')
+            }
+        }
+    }
+</script>
