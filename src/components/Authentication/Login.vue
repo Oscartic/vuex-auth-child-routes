5332af320ef6ee9f9ce8a312d90957caadc2c5e3
OscarB <oballesteros@abstract.cl>
2019-06-11 23:14:59 -0400

    add login form

---
+++ src/components/Authentication/Login.vue
@@ -0,0 +1,63 @@
+<template>
+    <b-form @submit.prevent="$emit('login')">
+        <b-form-group
+            label="Email"
+            description="Los campos son privados"
+        >
+            <b-form-input
+                type="email"
+                autocomplete="off"
+                v-model="user.email"
+                v-validate="'required|email'"
+                name="email"
+                placeholder="Introduce tu email">
+            </b-form-input>
+            <b-form-invalid-feedback>
+                {{ errors.first('email') }}
+            </b-form-invalid-feedback>
+        </b-form-group>
+
+         <b-form-group
+            label="Passworn"
+        >
+            <b-form-input
+                type="password"
+                autocomplete="off"
+                v-model="user.password"
+                v-validate="'required|min:6'"
+                name="Password"
+                placeholder="Introduce tu password">
+            </b-form-input>
+            <b-form-invalid-feedback>
+                {{ errors.first('password') }}
+            </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-button
+            :disabled="errors.any() || !user.password"
+            type="submit"
+            variant="primary"
+        >
+            Iniciar Sesión
+        </b-button>
+
+    </b-form>
+</template>
+
+<script>
+    export default {
+        props: {
+            user: {
+                type: Object,
+                required: true,
+                validator: user => {
+                    if (!user.hasOwnProperty('email') || !user.hasOwnProperty('password')) {
+                        console.warn('Usuario no válido')
+                        return false 
+                    }
+                    return true 
+                }
+            }
+        }   
+    }
+</script>
